9d151b15fc26aad4ffd98b1f0333a158fba5000a
zohaibsak <syedzohaibak@gmail.com>
2020-06-14 22:56:03 +0530

    Code Cleanup

--- functions/routes/xlsxToJson.js
+++ functions/routes/xlsxToJson.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
-let multipart = require("connect-multiparty");
-let multipartMiddleWare = multipart();
 const Excel = require("exceljs");
 const Stream = require("stream");
-// const firebase = require("firebase");
 
-// router.post("/", (req, res) => {
-//     return res.send(req.files);
-// });
 
 router.post("/getSheets", function (req, res) {
     let workbook = new Excel.Workbook();
@@ -27,7 +21,6 @@ router.post("/getSheets", function (req, res) {
 })
 
 router.post("/uploadFile", function (req, res) {
-    // return res.send(req.body);
     let inputValues = JSON.parse(req.body.inputValues);
    
     let sheetName = inputValues.sheetName;
@@ -67,33 +60,7 @@ router.post("/uploadFile", function (req, res) {
         .catch(function (error) {
             return "Error";
         });
-    // let workbook = new Excel.Workbook();
-    // var a;
-    // workbook.xlsx
-    //     .load(req.body)
-    //     .then(function() {
-    //         let worksheet = workbook.getWorksheet(1);
-    //         return res.send(worksheet.rowCount);
-    //         // let headerCollection = worksheet.getRow(1).values;
-    //         // let jsonCollection = [];
-    //         // for (let counter = 2; counter < worksheet.rowCount; counter++) {
-    //         //     let jsonObject = {};
-    //         //     let secondRow = worksheet.getRow(counter).values;
-    //         //     for (let i = 0; i < headerCollection.length; i++) {
-    //         //         if (headerCollection[i] !== undefined) {
-    //         //             if (secondRow[i] === undefined) {
-    //         //                 secondRow[i] = "";
-    //         //             }
-    //         //             jsonObject[headerCollection[i]] = secondRow[i];
-    //         //         }
-    //         //     }
-    //         //     jsonCollection.push(jsonObject);
-    //         // }
-    //         // return res.send(jsonCollection);
-    //     })
-    //     .catch(function() {
-    //         return res.send("Error");
-    //     });
+    
 });
 
 module.exports = router;
